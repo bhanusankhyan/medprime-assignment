1dae4cddab6952d2c9501795c6640c1708e61c49
bhanu <bhanusankhyan@live.com>
2021-02-10 17:56:56 +0530

    logic change

--- app.py
+++ app.py
@@ -5,12 +5,25 @@ app = Flask(__name__)
 
 
 def CountingItems(items):
-    result=  {
-        'countThis': items.count('this'),
-        'countThe': items.count('the'),
-        'countThere': items.count('there')
-    }
+    countThis = 0
+    countThe = 0
+    countThere = 0
+    for item in items:
+        print(item)
+        if 'this' in item.lower():
+            countThis += 1
+        elif 'there' in item.lower():
+            countThere += 1
+        elif 'the' in item.lower():
+            countThe += 1
+    result = {
+        'countThis': countThis,
+        'countThe' : countThe,
+        'countThere' : countThere
+        }
+    print(result)
     return result
+
 
 @app.route('/', methods = ['POST', 'GET'])
 def HomePage():
@@ -20,10 +33,16 @@ def HomePage():
         f = request.files['text-file']
         file = f.read().decode('utf-8')
         items = json.dumps(file)
-        result = CountingItems(items.lower())
-        print(result)
+        items = items.replace('[','')
+        items = items.replace(']','')
+        items = items.replace('\\n','')
+        items = items.replace('"','')
+        items = items.split(',')
+        print(type(items))
+        result = CountingItems(items)
         return jsonify(result = result)
 
+        
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
